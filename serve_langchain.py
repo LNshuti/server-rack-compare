dd99bd446589ce390b82a7139bc5c939a01c7f08
Leonce Nshuti <leonce.nshuti@sonymusicpub.com>
2024-05-24 12:38:45 -0500

    update requirements

--- serve_langchain.py
+++ serve_langchain.py
@@ -10,3 +10,27 @@ llm = VLLM(model="mosaicml/mpt-7b",
 )
 
 print(llm("What is the capital of France ?"))
+
+# from langchain_community.llms.fake import FakeStreamingListLLM
+# from langchain_core.output_parsers import StrOutputParser
+# from langchain_core.prompts import SystemMessagePromptTemplate
+# from langchain_core.runnables import Runnable
+# from operator import itemgetter
+
+# prompt = (
+#     SystemMessagePromptTemplate.from_template("You are a nice assistant.")
+#     + "{question}"
+# )
+# llm = FakeStreamingListLLM(responses=["foo-lish"])
+
+# chain: Runnable = prompt | llm | {"str": StrOutputParser()}
+
+# chain_with_assign = chain.assign(hello=itemgetter("str") | llm)
+
+# print(chain_with_assign.input_schema.schema())
+# # {'title': 'PromptInput', 'type': 'object', 'properties':
+# {'question': {'title': 'Question', 'type': 'string'}}}
+# print(chain_with_assign.output_schema.schema()) #
+# {'title': 'RunnableSequenceOutput', 'type': 'object', 'properties':
+# {'str': {'title': 'Str',
+# 'type': 'string'}, 'hello': {'title': 'Hello', 'type': 'string'}}}
